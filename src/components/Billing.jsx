15288b0e22bc430866e7dc447b8f0a3537fc44de
bloodykheeng <69955065+bloodykheeng@users.noreply.github.com>
2024-08-30 21:22:53 +0300

    elevate pesa

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,30 +1,101 @@
-import { apple, bill, google } from '../assets';
-import styles, { layout } from '../style';
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+// import ElevatePesaApp from "../assets/elevate-pesa/elevate-pesa-app-photo.jpg";
+import PesaFront from "../assets/elevate-pesa/pesa-front.jpg";
+import PesaOrders from "../assets/elevate-pesa/pesa-order.jpg";
 
 const Billing = () => (
-  <section id='product' className={layout.sectionReverse}>
+  <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
+      {/* <img
+        src={ElevatePesaApp}
+        alt="elevate-pesa"
+        className="w-[100%] h-[100%] relative z-[5]"
+      /> */}
 
-      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'></div>
-      <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'></div>
+      <div
+        style={{
+          height: "400px",
+          width: "100%",
+          overflow: "hidden",
+          position: "relative"
+        }}
+      >
+        <img
+          src={PesaFront}
+          alt="elevate-pesa"
+          style={{
+            // width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          height: "400px",
+          width: "100%",
+          overflow: "hidden",
+          position: "relative"
+        }}
+      >
+        <img
+          src={PesaOrders}
+          alt="elevate-pesa"
+          style={{
+            // width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0
+          }}
+        />
+      </div>
+
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
     </div>
 
     <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>
-        Easily control your <br className='sm:block hidden'/> billing & invoicing.
-      </h2>
+      <h2 className={styles.heading2}>Download Our App for Seamless Service</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+        Manage your gas, water, and package deliveries effortlessly with our
+        app. Available on both Apple and Google Play, it lets you order, track,
+        and manage your deliveries with ease. Download now and experience
+        hassle-free service!
       </p>
 
-      <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
-        <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
-        <img src={google} alt="play_store" className='w-[128px] h-[42px] object-contain cursor-pointer'/>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <a
+          href="https://apps.apple.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+        </a>
+        <a
+          href="https://play.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={google}
+            alt="play_store"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </a>
       </div>
     </div>
   </section>
-)
+);
 
-
-export default Billing
+export default Billing;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,62 @@
-import React, { useState } from 'react';
-import { close, logo, menu } from '../assets';
-import { navLinks } from '../constants';
+import React, { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+import PesaLogo from "../assets/elevate-pesa/pesa_with_green_bg.jpg";
 
 const Navbar = () => {
-
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-      
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32-px]' />
-      
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-        
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img
+        src={PesaLogo}
+        style={{ width: "60px" }}
+        alt="hoobank"
+        className="w-[124px] h-[32-px]"
+      />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] 
-              ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white hover:text-secondary transition-all`}
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] 
+              ${
+                index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              } text-white hover:text-secondary transition-all`}
           >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
-
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={toggle ? close : menu} alt="menu"
-          className='w-[28px] h-[28px] object-contain cursor-pointer'
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
           onClick={() => setToggle((prev) => !prev)}
         />
-          
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient 
+
+        <div
+          className={`${toggle ? "flex" : "hidden"} p-6 bg-black-gradient 
               absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-
-          <ul className='list-none flex flex-col justify-end items-center flex-1'>
-
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav, index) => (
-              <li key={nav.id} className={`font-poppins font-normal cursor-pointer 
-              text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer 
+              text-[16px] ${
+                index === navLinks.length - 1 ? "mr-0" : "mb-4"
+              } text-white`}
               >
-
-                <a href={`#${nav.id}`}>
-                  {nav.title}
-                </a>
-                
+                <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
-
           </ul>
-
         </div>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
